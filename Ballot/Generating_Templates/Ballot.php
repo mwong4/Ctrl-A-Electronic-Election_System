8f73b226d2bab980820fde1176ca9d63d5b6854e
MW <43390816+mwong4@users.noreply.github.com>
2023-01-12 12:09:15 -0500

    Working on readme

---
+++ Ballot/Generating_Templates/Ballot.php
@@ -0,0 +1,75 @@
+
+
+
+<?php
+
+    //HTML data (template)
+    $orig_HTML = <<<EOD
+
+    <!DOCTYPE html>
+    <html>
+    <title>
+        Ballot
+    </title>
+
+    <body>
+        <h1>This is the ballot! Multiple Votes Allowed!</h1>
+
+        <!-- form that is submitted via post -->
+        <form action="Submit.php" method="post">
+
+        <!-- repeating sections go here -->
+            <div>
+                <h3>president (Multi vote available)</h3>
+    
+                <p>Hi, my name is Joel. I code.</p>
+    
+                <p>Hello, my name is Sasha. I like art.</p>
+    
+            </div>
+    
+            <input type="checkbox" id="Joel" name="president[]" value="Joel">
+            <label for="Joel">Support Joel</label><br>
+    
+            <input type="checkbox" id="Sasha" name="president[]" value="Sasha">
+            <label for="Sasha">Support Sasha</label><br>
+    
+        
+            <div>
+                <h3>treasurer (Multi vote available)</h3>
+    
+                <p>Hey, I'm K. I really like anime... and maybe money.</p>
+    
+            </div>
+    
+            <input type="checkbox" id="K" name="treasurer[]" value="K">
+            <label for="K">Support K</label><br>
+    
+        
+            <div>
+                <h3>promotion manager (Multi vote available)</h3>
+    
+            </div>
+    
+        
+
+            <br><br>
+            <label for="u_id">Your user id: </label>
+            <!-- u_id stored here, passed on to back end with post call -->
+            <input type="text" id="u_id" name="u_id" value="null_id" readonly>
+            <br><br>
+            <input type="submit" value="Submit">
+            <br><br>
+            <form action="Submit.php" method="post">
+
+        </form>
+        <br>
+        <br>   
+    </body>
+    EOD;
+
+    // u_id from url embedded into template, then rendered
+    $u_id = $_GET['u_id'];
+    echo str_replace("null_id", $u_id, $orig_HTML);
+
+?>
--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Ctrl-A-EES
 Electronic Election System for Ctrl-A
+status: Prototype, in development
+
+###### General Information
+
+This is a electornic election system built for the Ctrl-A club at the University of Waterloo.
+
+## Settup
+
+Prior to usage, a few configurations must be completed. First, ensure that python is installed.
+```
+$ python -V
+> Python 3.10.5
+```
+
+next, ensure that all pip requirements have been met
+
+```
+$ cd /directory/where/files/are/located
+$ pip install -r requirements.txt
+```
+
+[with current prototype version, system is only functional on localhost]
+Use xampp to host the dynamic website and database
+
+###### json configuration
+
+Before ballots can be distributed, they must be generated from a config file, written in json. Below is an example:
+
+```
+{
+    "president": {
+        "Joel": {
+            "description": "Hi, my name is Joel. I code."
+        },
+        "Sasha": {
+            "description": "Hello, my name is Sasha. I like art."
+        }
+    },
+    "treasurer": {
+        "K": {
+            "description": "Hey, I'm K. I really like anime... and maybe money."
+        }
+    },
+    "promotion manager": {
+
+    }
+}
+```
+
+In the example, we have a position of "president" with two individuals running for the position. "Treasurer" has 1 person running and "Promotional Manager" has none.
+Each individual also has a custom description. Please write this file, and ensure that it is in ./Ballot/Generating_Templates.
+
+Once ready, run the python script to generate the ballot:
+```
+$ cd ./Ballot/Generating_Templates
+$ py Generate.py <input_file.json>
+> Generating...
+> Ballot generated!
+```
+
+With this command, a Ballot.php will apear in this directory. Open this file in a browser to confirm the layout is correct, then move the file to ./Ballot
+
+Now, the ballot is ready!
+
+###### Script configurations
+
+
+## Usgae
