12267165f81274024d1a57c469a9d321943e64c8
MW <43390816+mwong4@users.noreply.github.com>
2023-01-23 16:17:44 -0500

    Initial port of theme from Square squared

--- Webfront/Initial.php
+++ Webfront/Initial.php
@@ -1,15 +1,46 @@
 <!DOCTYPE html>
-<html>
-<title>
-    Submission
-</title>
 
-<body>
-    <h2>Please Enter Wat-Email. If valid, your Ballot will be Emailed to your Waterloo Address. Please Check SPAM Folder</h2>
-    <!-- basic form that gets email, then formwards this to next page -->
-    <form action="Result.php" method="post"> 
-        E-Mail: <input type="text" placeholder="jdoe@waterloo.ca" name="EMAIL" id="email "><br>
-        <input type="submit" value="submit" name="SUBMIT">
-    </form>
-</body>
+<html>
+<!-- Header -->
+	<head>
+		<title>Ctrl A EES | Submission</title>
+		
+		<link rel="stylesheet" type="text/css" href="../styles.css">
+	</head>
+
+  <!-- body -->
+	<body>
+  
+		<!-- header -->
+		<div class="secondaryHeader">
+
+			<!-- the navigation bar -->
+ 			<div class="topnav">
+				<img src="../logosquaresuqaredgooderWHITE.png" alt="temporary plant logo" height="42" width="42" id="logo">
+				<div id="topGradient"></div>
+				<a href="Initial.php" id="homeButton">Submission</a>
+				<a href="../Results/Display.php">Results</a>
+			</div>
+			<h1 class="secondaryHeaderText">Submission</h1>
+				<div id="#secondaryMiddleGradient"></div>
+		</div>
+		
+
+		<div id="bottomGradient"></div>
+		
+		<div class="headerTheme">
+			<h2 class="secondHeader"> Please Enter Wat-Email. Ballots emailed, check SPAM! <h2>
+		</div>
+		
+		<div class="container">
+				<form action="Result.php", method="post">
+		 
+				<label for="fname">Waterloo Email</label>
+				<input type="text" id="fname" name="EMAIL" placeholder="jdoe@waterloo.ca">
+                <input type="submit" value="submit" name="SUBMIT">
+				<!-- <a style="text-decoration:none" class="button" >Submit</a> -->
+				<!--<button id="submitBtn" onclick="SubmitClick"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>-->
+			 </form>
+			</div>
+	</Body>
 </html>
