48647359a342e1790475934412d4558b598fdeb6
MW <43390816+mwong4@users.noreply.github.com>
2023-01-24 09:37:58 -0500

    Testing out chart.js
    Need to implement my own integration of chart.js

--- Results/Display.php
+++ Results/Display.php
@@ -33,7 +33,36 @@
             $result = exec("python Results.py"); //calls back end to get current results, then displays them
             echo $result;
           ?>
-		    </p>
+		</p>
+
+		<!-- Source: https://chartscss.org/charts/bar/ -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+		<canvas id="myChart" style="width:100%;max-width:40%;margin:auto;"></canvas>
+		
+		<script>
+		var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+		var yValues = [55, 49, 44, 24, 15];
+		var barColors = ["red", "green","blue","orange","brown"];
+
+		new Chart("myChart", {
+		type: "bar",
+		data: {
+			labels: xValues,
+			datasets: [{
+			backgroundColor: barColors,
+			data: yValues
+			}]
+		},
+		options: {
+			legend: {display: false},
+			title: {
+			display: true,
+			text: "World Wine Production 2018",
+			color: '#FFF'
+			}
+		}
+		});
+		</script>
         
         <br><br><br>
         <div class="footer">
